351a71b4b1a0e9d467937aad1b14f94fb024afa1
HugoWalu <dustin_2@gmx.de>
2026-08-31 16:08:06 +0200

    5: Running Game

--- actions.py
+++ actions.py
@@ -2,9 +2,9 @@ from queue import Queue
 from cards import Card, unshuffled_deck
 import random
 
-middle_pile = Queue()
 my_deck = Queue()
 opponent_deck = Queue()
+middle_pile = Queue()
 
 def prep_decks(unshuffled_deck):
     random.shuffle(unshuffled_deck)
@@ -24,7 +24,7 @@ def status_top_card(deck = my_deck):
 def check_deck_size(d1 = my_deck, d2 = opponent_deck)-> None:
     return f"My deck size: {d1.size()}\nOppenent deck size:{d2.size()}"
 
-def compare_stats(d1 = my_deck, d2 = opponent_deck, pile = middle_pile):
+def compare_stats(d1 = my_deck, d2 = opponent_deck, pile = middle_pile)-> str:
     x = input("Enter a stat: ").strip().lower()
 
     stat_map = {
@@ -47,6 +47,7 @@ def compare_stats(d1 = my_deck, d2 = opponent_deck, pile = middle_pile):
     attr = stat_map.get(x)
     if attr is None:
         print("Invalid stat")
+        compare_stats()
     else:
         astat = getattr(a, attr)
         bstat = getattr(b,attr)
@@ -55,6 +56,7 @@ def compare_stats(d1 = my_deck, d2 = opponent_deck, pile = middle_pile):
         print(f"My {a.name} with {x} {astat} tied against your {b.name} with {x} {bstat}")
         pile.push(a)
         pile.push(b)
+        return "Tie"
     elif (astat > bstat):
         print(f"My {a.name} with {x} {astat} wins against your {b.name} with {x} {bstat}")
         d1.push(a)
@@ -63,6 +65,7 @@ def compare_stats(d1 = my_deck, d2 = opponent_deck, pile = middle_pile):
             for i in range(0,pile.size()):
                 tmp = pile.pop()
                 d1.push(tmp)
+        return "Win"
     else:
         print(f"My {a.name} with {x} {astat} loses against your {b.name} with {x} {bstat}")
         d2.push(b)
@@ -71,6 +74,7 @@ def compare_stats(d1 = my_deck, d2 = opponent_deck, pile = middle_pile):
             for i in range(0,pile.size()):
                 tmp = pile.pop()
                 d2.push(tmp)
+        return "Lose"
 
 
 def main():
--- game.py
+++ game.py
@@ -1,8 +1,41 @@
-import random
 from queue import Queue
 from cards import *
+from actions import *
 
-shuffled_deck = random.shuffle(deck)
-my_deck = Queue()
-opponent_deck = Queue()
-middle_pile = Queue()
+
+def supertrumpf(d1 = my_deck, d2 = opponent_deck):
+    prep_decks(unshuffled_deck)
+    while (d1.size() != 0 and d2.size() != 0):
+        x = input("Check Decksize, Compare Stats, Check Topcard").strip().lower()
+
+        action_map = {
+            "size": "size",
+            "decksize": "size",
+            "check decksize": "size",
+            "stats": "stats",
+            "compare stats": "stats",
+            "card": "card",
+            "check card": "card",
+            "check topcard": "card",
+            "topcard": "card"
+        }
+        action = action_map.get(x)
+        if action == None:
+            print("Invalid Action")
+        elif action == "size":
+            print(check_deck_size())
+        elif action == "card":
+            print(status_top_card())
+        else:
+            compare_stats()
+    if d1.size == 0:
+        print("You lose!")
+        return
+    else:
+        print("You win!")
+        return
+
+def main():
+    supertrumpf()
+
+main()
